928b01170b4fc8b72f709816e7a49c1091416386
NiBo🧛‍ <nikosmpountalis@gmail.com>
2026-01-10 16:46:06 +0200

    fixed base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://nikosmpountalis.github.io/My-portfolio/',
+  base: '/My-portfolio/',
   build: {
     outDir: 'dist',
   }
